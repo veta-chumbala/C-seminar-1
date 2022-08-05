809b738576c982252e1303d3525a0c1eaf0c12ff
Elizaveta Lvova-Kirisik <e.kirisik@yandex.ru>
2022-08-05 14:35:55 +0300

    Done Task5 and Task7

---
+++ Task5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Напишите целое положительное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = -num;
+if (num > 0)
+{while (i <= num)
+    {
+        Console.Write($" {i}");
+        i += 1;
+    }
+}
+else Console.WriteLine("Введено неверное число");
---
+++ Task7/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Напишите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number < 1000)
+{Console.Write($"Последняя цифра числа {number} - это {number % 10}");}
+else {Console.WriteLine("Введено неверное число.");}
