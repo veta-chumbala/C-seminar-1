a9fc5ae4b471fc959570847e7414d89898029dc1
Elizaveta Lvova-Kirisik <e.kirisik@yandex.ru>
2022-08-05 13:00:01 +0300

    Done Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите первое число: ");
+int first_num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int second_num = Convert.ToInt32(Console.ReadLine());
+if (second_num * second_num == first_num) 
+{Console.WriteLine($"{first_num} является квадратом {second_num}");}
+else
+{Console.WriteLine($"{first_num} не является квадратом {second_num}");}
