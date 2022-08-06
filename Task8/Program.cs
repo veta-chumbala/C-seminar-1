9243a11f22990d0e96aceb04fca7bd09ed8ed4a0
Elizaveta Lvova-Kirisik <e.kirisik@yandex.ru>
2022-08-06 16:40:04 +0300

    Done home-task8

---
+++ Task8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Напишите целое положительное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+if (num > 0)
+{while (i <= num)
+    {
+        Console.Write($"{i} ");
+        i += 2;
+    }
+}
+else Console.WriteLine("Введено неверное число");
