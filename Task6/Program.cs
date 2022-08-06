9e8c41d15e431aab16c54aa35bfad2b7ab050a2a
Elizaveta Lvova-Kirisik <e.kirisik@yandex.ru>
2022-08-06 16:21:14 +0300

    Done home-task6

---
+++ Task6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Напишите свое число:");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num%2 == 0) {
+    Console.WriteLine("да");
+}
+else {
+    Console.WriteLine("нет");
+}
