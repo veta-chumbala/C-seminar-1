2faef7218347856ac0b3c4c88a11602aecf02115
Elizaveta Lvova-Kirisik <e.kirisik@yandex.ru>
2022-08-06 16:13:08 +0300

    Done home-task4

---
+++ Task4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Напишите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Напишите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Напишите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+Console.WriteLine($"{max}");
